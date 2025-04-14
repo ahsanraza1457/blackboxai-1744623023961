1dd56c86ebae17752fdfdef4259c9492667c272e
Ahsan Raza <ahsanraza1457@gmail.com>
2025-04-14 15:41:30 +0500

    Add kissan-ghar/admin/js/admin.js 1744627289641

--- kissan-ghar/admin/js/admin.js
+++ kissan-ghar/admin/js/admin.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'http://127.0.0.1:5000/api';
 
 // Utility Functions
 async function makeApiRequest(endpoint, method = 'GET', data = null) {
